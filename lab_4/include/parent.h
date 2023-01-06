b5ecc712211be5f396292f31f20316dce89b90d6
kiviyi <90795245+kiviyi@users.noreply.github.com>
2023-01-06 17:02:52 +0300

    Add files via upload

---
+++ lab_4/include/parent.h
@@ -0,0 +1,9 @@
+#ifndef LAB2_PARENT_H
+#define LAB2_PARENT_H
+
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> ParentRoutine(const std::vector<std::string> &input);
+
+#endif //LAB2_PARENT_H
